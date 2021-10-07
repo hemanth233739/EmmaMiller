a1c907a5836f91268352ec97e609d49981affb22
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-10-07 18:33:39 +0530

    Update misc.py

--- EmmaMiller/modules/misc.py
+++ EmmaMiller/modules/misc.py
@@ -656,7 +656,7 @@ __help__ = """
 \
 """
 
-__mod_name__ = "Miscs"
+__mod_name__ = "💠 Miscs 💠"
 
 APP_HANDLER = DisableAbleCommandHandler("app", app)
 LYRICS_HANDLER = DisableAbleCommandHandler("lyrics", lyrics, pass_args=True)
