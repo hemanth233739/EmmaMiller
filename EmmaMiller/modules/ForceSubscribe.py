bef7590d3a45ba40e53c2569576cc6a0bea2f46c
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-10-07 17:53:59 +0530

    Update ForceSubscribe.py

--- EmmaMiller/modules/ForceSubscribe.py
+++ EmmaMiller/modules/ForceSubscribe.py
@@ -194,4 +194,4 @@ Note: Only creator of the group can setup me and i will not allow force subscrib
 ⚠️ Note:  /FSub is an alias of /ForceSubscribe
 
 """
-__mod_name__ = "F-Sub"
+__mod_name__ = "💠 F-Sub 💠"
