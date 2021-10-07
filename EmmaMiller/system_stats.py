66ac9b786510efc882f74bcf362323607f99ae0b
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-10-07 20:54:40 +0530

    Update system_stats.py

--- EmmaMiller/system_stats.py
+++ EmmaMiller/system_stats.py
@@ -23,7 +23,7 @@ async def bot_sys_stats():
     disk = psutil.disk_usage("/").percent
     process = psutil.Process(os.getpid())
     stats = f"""
-root@Supunma:~$ szrosebot
+root@mkspali:~$ EmmaMillerBot
 ------------------
 🌷UPTIME: {formatter.get_readable_time((bot_uptime))}
 🌷BOT: {round(process.memory_info()[0] / 1024 ** 2)} MB
