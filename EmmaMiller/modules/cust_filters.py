3cf2fdf6d2e0c48c9c32923574073231ead87203
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-10-07 17:58:01 +0530

    Update cust_filters.py

--- EmmaMiller/modules/cust_filters.py
+++ EmmaMiller/modules/cust_filters.py
@@ -633,7 +633,7 @@ Check `/markdownhelp` to know more!
 
 """
 
-__mod_name__ = "Filters"
+__mod_name__ = "💠 Filter 💠"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
