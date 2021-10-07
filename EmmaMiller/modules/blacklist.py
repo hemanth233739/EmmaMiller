45f3604ec57a141974f0fa8d128eede494a4aaa8
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-10-07 21:38:24 +0530

    Update blacklist.py

--- EmmaMiller/modules/blacklist.py
+++ EmmaMiller/modules/blacklist.py
@@ -452,7 +452,7 @@ def __stats__():
 __mod_name__ = "💠 Blacklist 💠"
 
 __help__ = """
-@szrosebot🇱🇰
+@BotMasterOfficial
 Blacklists are used to stop certain triggers from being said in a group. Any time the trigger is mentioned, the message will immediately be deleted. A good combo is sometimes to pair this up with warn filters!
 *NOTE*: Blacklists do not affect group admins.
  ❍ `/blacklist`*:* View the current blacklisted words.
