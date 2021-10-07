2f7ca6999c4a7ba249f13bdd65b2127a8b5701db
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-10-07 19:56:08 +0530

    Update __repo.py

--- EmmaMiller/modules/__repo.py
+++ EmmaMiller/modules/__repo.py
@@ -5,12 +5,12 @@ from pyrogram.types import *
 from EmmaMiller.config import get_str_key
 from EmmaMiller import pbot
 
-REPO_TEXT = "**A Powerful BOT to Make Your Groups Secured and Organized ! \n\n↼ Øwñêr ⇀ : 『 [Mukesh Solanki](https://t.me/mkspali) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Resently\n╰──────────────\n\n»»» @BotMasterOfficial «««"
+REPO_TEXT = "**A Powerful [BOT](https://telegra.ph/file/012ca899d61231500b90d.jpg) to Make Your Groups Secured and Organized ! \n\n↼ Øwñêr ⇀ : 『 [Mukesh Solanki](https://t.me/mkspali) 』\n╭──────────────\n┣─ » Python ~ 3.8.6\n┣─ » Update ~ Recently\n╰──────────────\n\n»»» @BotMasterOfficial «««"
   
 BUTTONS = InlineKeyboardMarkup(
       [[
         InlineKeyboardButton("💠 Repository 💠", url=f"https://github.com/BotMasterOfficial/EmmaMiller"),
-        InlineKeyboardButton("💠 Joint 💠", url=f"https://t.me/RMCMG"),
+        InlineKeyboardButton("💠 Join 💠", url=f"https://t.me/RMCMG"),
       ],[
         InlineKeyboardButton("💠 Bot Master 💠", url="https://t.me/BotMaster_mkspali"),
         InlineKeyboardButton("💠 Support 💠", url="https://t.me/BotMasterOfficial"),
