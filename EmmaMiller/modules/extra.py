281b89bb91615fb781147c5367f031fde265e1c6
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-10-07 19:44:35 +0530

    Update extra.py

--- EmmaMiller/modules/extra.py
+++ EmmaMiller/modules/extra.py
@@ -1,14 +1,14 @@
 __help__ = """
-@szrosebot🇱🇰
+@BotMasterOfficial
 
 📝CAPTION EDITOR 
-now you can edit any file caption useing szrosebot .
+now you can edit any file caption useing [Emma Miller](https://t.me/EmmaMillerBot) .
 ❍/cedit <reply to file > < new caption name> :- caption editor 
 
 🔗URL SHORNER 
-Send the Long URL and get a Short URL Easily via szrosebot use this format 
+Send the Long URL and get a Short URL Easily via [Emma Miller](https://t.me/EmmaMillerBot) use this format 
 
- ❍ /short <your url> :-  you can get short url useing szrosebot 
+ ❍ /short <your url> :-  you can get short url useing Emma Miller
  
 """
-__mod_name__ = "🍁Extra Commands 🍁"
+__mod_name__ = "💠 Extra 💠"
