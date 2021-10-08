1a6300025857527a130398c2fda7c37c8da4c209
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-10-08 10:31:24 +0530

    Update yt.py

--- EmmaMiller/modules/yt.py
+++ EmmaMiller/modules/yt.py
@@ -1,5 +1,5 @@
 __help__ = """
-@szrosebot🇱🇰
+@BotMasterOfficial
  ❍ /yt <text>: perform a youtube search
  ❍ /ytaudio <link> or /ytvideo <link>: Downlods a video or audio from a youtube video to the bots local server
 """
