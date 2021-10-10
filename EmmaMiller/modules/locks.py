8bdf22c694c52a0512ead8d8ee44b97d6988773a
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-10-10 11:09:51 +0530

    Update locks.py

--- EmmaMiller/modules/locks.py
+++ EmmaMiller/modules/locks.py
@@ -578,7 +578,7 @@ Locking bots will stop non-admins from adding bots to the chat.
  
 """
 
-__mod_name__ = "💠 Locks 💠"
+__mod_name__ = "☠𝐋𝐨𝐜𝐤𝐬☠"
 
 LOCKTYPES_HANDLER = DisableAbleCommandHandler("locktypes", locktypes)
 LOCK_HANDLER = CommandHandler("lock", lock, pass_args=True)  # , filters=Filters.group)
