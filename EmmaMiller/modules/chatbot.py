ff713913a5dfbf74d172ada936a4ef49966ee77a
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-10-10 10:35:35 +0530

    Update chatbot.py

--- EmmaMiller/modules/chatbot.py
+++ EmmaMiller/modules/chatbot.py
@@ -147,7 +147,7 @@ Chatbot utilizes the Kuki's api which allows Sophia to talk and provide a more i
 *Powered by ItelAi* (https://github/itelai) from @KukiUpdates
 """
 
-__mod_name__ = "💠 ChatBot 💠"
+__mod_name__ = "☠𝐂𝐡𝐚𝐭𝐁𝐨𝐭☠"
 
 
 CHATBOTK_HANDLER = CommandHandler("chatbot", kuki)
