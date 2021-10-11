c6ff3b6d2f838a99a488c90ea4b00a5dcc6ee3e1
Mukesh Solanki <91374635+BotMasterOfficial@users.noreply.github.com>
2021-10-11 19:07:06 +0530

    Update misc.py

--- EmmaMiller/modules/helper_funcs/misc.py
+++ EmmaMiller/modules/helper_funcs/misc.py
@@ -67,11 +67,11 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if len(pairs) > 9:
         pairs = pairs[modulo_page * 9:9 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("☜", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("۞", callback_data="layla_back"),
+                EqInlineKeyboardButton("۞", callback_data="emma_back"),
              EqInlineKeyboardButton("☞", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Back", callback_data="layla_back")]]
+        pairs += [[EqInlineKeyboardButton("Back", callback_data="emma_back")]]
 
     return pairs
 
